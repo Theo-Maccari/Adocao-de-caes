7993866a5250e8bf2c4f8b5071b82837b5c5f77e
theo-maccari <theo_maccari@estudante.sesisenai.org.br>
2026-04-09 08:54:23 -0300

    up - index e lógica corrigidos

--- logic.js
+++ logic.js
@@ -5,8 +5,8 @@ document.getElementById("forms").addEventListener("submit", function (e) {
     let idade = document.getElementById("idade").value;
     let cpf = document.getElementById("cpf").value;
     let email = document.getElementById("email").value;
-    let telefone = document.getElementById("email").value;
-    let cidade = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let cidade = document.getElementById("cidade").value;
     let moradia = document.getElementById("moradia").value;
     let quintal = document.getElementById("quintal").value;
     let pet = document.getElementById("pet").value;
